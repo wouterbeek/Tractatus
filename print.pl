b030052759c3cd2e47daaddaebd759e8bf3b1022
Wouter Beek <me@wouterbeek.com>
2016-02-25 00:25:40 +0100

    ADDED: Org mode exporter.

--- print.pl
+++ print.pl
@@ -1,8 +1,9 @@
 :- module(
   print,
   [
+    org_mode_export/1, % +Lang
     print_tree/0,
-    print_tree/1  % ?Lang
+    print_tree/1       % ?Lang
   ]
 ).
 
@@ -12,12 +13,28 @@
 @version 2016/02
 */
 
+:- use_module(library(dcg/basics)).
 :- use_module(library(dcg/dcg_content)).
 :- use_module(library(dcg/dcg_phrase)).
 :- use_module(library(dcg/dcg_tree)).
 :- use_module(library(string_ext)).
 
 :- use_module(tree).
+
+
+
+
+
+%! org_mode_export(+Lang) is det.
+
+org_mode_export(Lang) :-
+  tree(Tree),
+  file_name_extension(Lang, org, File),
+  setup_call_cleanup(
+    open(File, write, Sink),
+    print_tree0(Sink, Tree, org_writer(Lang)),
+    close(Sink)
+  ).
 
 
 
@@ -32,14 +49,37 @@ print_tree :-
 
 print_tree(Lang) :-
   tree(Tree),
-  dcg_with_output_to(current_output,
-    dcg_tree(Tree, [node_writer(print_node(Lang))])
+  print_tree0(Tree, node_writer(Lang)).
+
+
+
+% HELPERS %
+
+print_tree0(Tree, NodeWriter) :-
+  print_tree0(current_output, Tree, NodeWriter).
+
+print_tree0(Sink, Tree, NodeWriter) :-
+  dcg_with_output_to(Sink,
+    dcg_tree(Tree, [node_writer(NodeWriter)])
   ).
 
+org_writer(Lang, [Node|InvPath], _) -->
+  {
+    reverse([Node|InvPath], [_|Path]),
+    (proposition(Lang, Path, S) -> true ; S = "∅")
+  },
+  stars([Node|InvPath]), " ",  path(Path), " ", str(S).
 
-%! print_node(+Lang, +InvPath, +IsLeaf)// is det.
+path([]) --> !, "".
+path([H|T]) --> integer(H), ({T == []} -> "" ; ".", integers(T)).
 
-print_node(Lang, [Node|InvPath], _) -->
+integers([]) --> !, "".
+integers([H|T]) --> integer(H), integers(T).
+
+stars([]) --> !, "".
+stars([_|T]) --> "*", stars(T).
+
+tree_writer(Lang, [Node|InvPath], _) -->
   {
     reverse([Node|InvPath], [_|Path]),
     (proposition(Lang, Path, S) -> true ; S = "∅"),
